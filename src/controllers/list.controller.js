8943369d67075658e5f7c8a2df10e42da0425fbb
Wilmer Correa <wcorrea@dom_invertec.invertec.cl>
2022-08-29 17:11:19 -0400

    Cambio en el puerto del servidor web

--- src/controllers/list.controller.js
+++ src/controllers/list.controller.js
@@ -13,16 +13,15 @@ export const createNewLog = async (req,res) => {
 
     let { Fecha,Producto, O_F } = req.body
 
-    
     if ( Fecha === null || Producto === null || O_F === null ){
         return res.status(400).json({msg:'Bad Request.'})
     }
     
     const pool = await getConnection()
     await pool.request()
-    .input("Fecha", sql.Date, '2022-08-29')
-    .input("Producto",sql.VarChar,"Jugo")
-    .input("O_F",sql.VarChar,"545569")
+    .input("Fecha", sql.Date, "2022-08-28")
+    .input("Producto",sql.VarChar,"Jugo2")
+    .input("O_F",sql.VarChar,"5588899")
     .query('INSERT INTO Tabla_datos (Fecha,Producto,O_F) VALUES (@Fecha,@Producto,@O_F)')
 
     res.json('New Log')
