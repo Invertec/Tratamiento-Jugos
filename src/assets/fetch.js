ac3bc87e2fe4fb48fcaa2cfe3a82707f75b5f6c6
Wilmer Correa <wcorrea@dom_invertec.invertec.cl>
2022-08-30 16:37:04 -0400

    Configurando Boton crear

--- src/assets/fetch.js
+++ src/assets/fetch.js
@@ -1,17 +1,15 @@
-// let url = 'http://localhost:3000/list'
-// fetch(url )
-//     .then( response => response.json() )
-//     .then( data => mostrarData(data) )
-//     .catch(error => console.log(error))
+const url = 'http://localhost:3000/list'
+const contenedor = document.querySelector('tbody')
+let resultados = ''
 
+const modalArticulo = new bootstrap.Modal(document.getElementById('modalArticulo'));
+const formArticulo = document.querySelector('form')
+const Fecha = document.getElementById('Fecha')
+const Producto = document.getElementById('Producto')
+const OrdenFabricacion = document.getElementById('OrdenFabricacion')
+const Observacion = document.getElementById('Observacion')
+let opcion = ''
 
-
-// const mostrarData = (data) => {
-//     console.log(data);
-//     let body = ''
-//     for (let i =0; i < data.length;i++){
-//     body +=   `<tr><td>${data[i].id}</td><td>${data[i].Fecha}</td><td>${data[i].Producto}</td><td>${data[i].OrdenFabricacion}</td></tr>`
-//     }
-
-//     document.getElementById('data').innerHTML = body
-// }
+btnCrear.addEventListener('click', () => {
+    modalArticulo.show()
+})
--- src/routes/list.rout.js
+++ src/routes/list.rout.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { getList, createNewLog, getHtml } from "../controllers/list.controller";
+import { getList, createNewLog} from "../controllers/list.controller";
 
 const router = Router()
 
-
-router.get('/', getHtml)
 router.get('/list',getList )
 router.post('/list',createNewLog )
 
