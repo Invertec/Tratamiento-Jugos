0c2b804a4eb741d7a7a192de6f0265c9e118247b
Wilmer Correa <wcorrea@dom_invertec.invertec.cl>
2022-08-31 17:04:22 -0400

    Mostrando Datos en la tabla

--- src/assets/fetch.js
+++ src/assets/fetch.js
@@ -13,3 +13,22 @@ let opcion = ''
 btnCrear.addEventListener('click', () => {
     modalArticulo.show()
 })
+
+const mostrar = (articulos) => {
+    articulos.forEach(articulo => {
+        resultados += `
+                    <tr>
+                        <td>${articulo.id}</td>
+                        <td>${articulo.Fecha}</td>
+                        <td>${articulo.Producto}</td>
+                        <td>${articulo.OrdenFabricacion}</td>
+                    </tr> `
+    })
+    contenedor.innerHTML = resultados
+}
+
+// Mostrar datos
+fetch(url)
+    .then( response => response.json() )
+    .then( data => mostrar(data) )
+    .catch( error => console.log(error) )
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from './app.js'
 
 app.listen(app.get('port'))
 
-console.log('server on port', app.get('port'));
+console.log('server on http://localhost:'+ app.get('port'));
